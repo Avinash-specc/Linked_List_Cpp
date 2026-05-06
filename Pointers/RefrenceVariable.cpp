c8f07032e1df4a3ba44c2e9c6bf6746ac26b1ef0
Avinash Kushwaha <avinashkushwaha7265@gmail.com>
2026-05-06 18:08:15 +0530

    Dynamic Allocation

--- Pointers/RefrenceVariable.cpp
+++ Pointers/RefrenceVariable.cpp
@@ -34,8 +34,23 @@ int main(){
 
     //Why do we need Refrence Variable??
 
-    int n =5;
-    update(n); //while generating a refrence variable original value is also changed.
-    cout<<n;
+    // int n =5;
+    // update(n); //while generating a refrence variable original value is also changed.
+    // cout<<n;
+
+
+    // int arr[100]; //this type of array declaration is considered as a bad practice as memory alloted to the array is through stack and not heap thus declaring an array of larger size will crash the program.It is known as static allocation
+
+    //To use the heap memory we use keyword 'new'.It is known as Dynamic memory Allocation.
+
+    // char* ch = new char('A'); //declaring pointer ch to point dynamically allocated char.
+
+    char cha = 'A';
+    char* c = &cha;
+
+    cout<<sizeof(cha)<<endl;
+    cout<<sizeof(c)<<endl;
+
+    int* i = new int;
     return 0;
 }
