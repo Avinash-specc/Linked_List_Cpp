49bdc35e9ce023a965223e4e82c2521363ee6a6f
Avinash Kushwaha <avinashkushwaha7265@gmail.com>
2026-06-20 23:22:45 +0530

    Multiple Inheritance

---
+++ Oop/multiple_Inheritance.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+class Animal{
+    public:
+    int age;
+    int weight;
+
+    public:
+    void bark(){
+        cout<<"Barking."<<endl;
+    }
+};
+
+class Dog: public Animal{
+    
+};
+
+class GermanShepherd: public Dog{
+
+};
+
+class Human{
+    public:
+    string color;
+    public:
+    void speak(){
+        cout<<"Speaking "<<endl;
+    }
+};
+
+class Hybrid: public Animal, public Human{
+
+};
+int main(){
+    Hybrid obj1;
+    obj1.speak();
+    obj1.bark();
+    return 0;
+}
